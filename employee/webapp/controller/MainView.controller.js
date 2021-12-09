7ceef9b4822791ccf2ebbc221b4c34ac6369fe19
victortsuchida <victortsuchida@gmail.com>
2021-12-09 20:56:25 +0000

    last commit

--- employee/webapp/controller/MainView.controller.js
+++ employee/webapp/controller/MainView.controller.js
@@ -1,46 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 * @param {typeof sap.m.MessageToast} MessageToast
 	 * @param {typeof sap.ui.model.json.JSONModel} JSONModel
 	 */
-    function (Controller, MessageToast, JSONModel) {
+    function (Controller, MessageToast, JSONModel, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("logaligroup.employee.controller.MainView", {
             onInit: function () {
-/*                let oDataCountries = {
-                    countries: [
-                        {
-                            key: "",
-                            country: ""
-                        },                        
-                        {
-                            key: "US",
-                            country: "United States"
-                        },
-                        {
-                            key: "UK",
-                            country: "United Kingdom"
-                        },
-                        {
-                            key: "ES",
-                            country: "Spain"
-                        }
-                    ]
-                };
-
-
-
-                let oModCountries = new JSONModel(oDataCountries);*/
-                //this.getView().setModel(oModCountries, "countries");
-
                 let oDataVis = {
-                    visibility: false
+                    visibility: true
                 };
 
                 // Model for visibility
@@ -68,6 +44,37 @@ sap.ui.define([
                     //oInput.setDescription("Not OK");
                     oModelVis.setProperty("/visibility", false);
                 }
+            },
+
+            onFilter: function(oEvent){
+                let sCountryKey = this.getView().getModel("employee").getProperty("/CountryKey"),
+                    sEmployeeId = this.getView().getModel("employee").getProperty("/EmployeeId"),
+
+                    oCountryFilter = new Filter("Country", FilterOperator.EQ, sCountryKey),
+                    oEmployeeFilter = new Filter("EmployeeID", FilterOperator.Contains, sEmployeeId),
+                    oList = this.getView().byId("_IDGenTable1").getBinding("items"),
+                    aFilters = [];
+
+                aFilters.push(oCountryFilter);
+                aFilters.push(oEmployeeFilter);
+
+                oList.filter(aFilters);
+            },
+
+            onClearFilter: function(oEvent){
+                let oList = this.getView().byId("_IDGenTable1").getBinding("items"),
+                    aFilters = [],
+                    oModel = this.getView().getModel("employee");
+
+                oList.filter(aFilters);
+
+                oModel.setProperty("/CountryKey", "");
+                oModel.setProperty("/EmployeeId", "");
+            },
+
+            onShowPostalCode: function(oEvent){
+                let oItem = oEvent.getSource().getBindingContext("employee").getObject();
+                MessageToast.show(oItem.PostalCode)
             }
         });
     });
